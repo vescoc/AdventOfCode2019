44e7e6fff07dcfd8af8d9748d450e39220e18513
vescoc <claudio.vesco@gmail.com>
2019-12-03 12:41:06 +0100

    refactoring

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -6,27 +6,60 @@ extern crate lazy_static;
 
 use std::cmp;
 use std::collections::{HashMap, HashSet};
+use std::convert;
+use std::ops;
 
-type Point = (i32, i32);
+#[derive(PartialEq, Hash)]
+pub struct Point(i32, i32);
+
+impl Point {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self(x, y)
+    }
+
+    pub fn abs(&self) -> u32 {
+        (self.0.abs() + self.1.abs()) as u32
+    }
+}
+
+impl ops::Sub for &Point {
+    type Output = Point;
+
+    fn sub(self, rhs: &Point) -> Point {
+        Point::new(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl convert::From<(i32, i32)> for Point {
+    fn from((x, y): (i32, i32)) -> Self {
+        Self::new(x, y)
+    }
+}
+
+impl cmp::Eq for Point {}
+
 type Path = (HashSet<Point>, HashMap<Point, u32>);
+
+const SEPARATOR: char = ',';
 
 lazy_static! {
     pub static ref DATA: Vec<Path> = include_str!("../data.txt")
         .lines()
-        .map(|l| make_path(l.trim().split(',')))
+        .map(|l| make_path(l.trim().split(SEPARATOR)))
         .collect();
+    pub static ref ORIGIN: Point = Point::new(0, 0);
 }
 
-pub fn manhattan_distance(p1: Point, p2: Point) -> u32 {
-    ((p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()) as u32
+pub fn manhattan_distance(p1: &Point, p2: &Point) -> u32 {
+    (p1 - p2).abs()
 }
 
 pub fn closest_intersection<'a>(path1: &'a Path, path2: &'a Path) -> &'a Point {
-    let mut intersections = path1.0.intersection(&path2.0).collect::<Vec<&Point>>();
-
-    intersections.sort_by(|a, b| (a.0.abs() + a.1.abs()).cmp(&(b.0.abs() + b.1.abs())));
-
-    intersections[0]
+    path1
+        .0
+        .intersection(&path2.0)
+        .min_by(|a, b| (a.abs()).cmp(&(b.abs())))
+        .unwrap()
 }
 
 pub fn better_intersection<'a>(path1: &'a Path, path2: &'a Path) -> (&'a Point, u32) {
@@ -59,9 +92,9 @@ pub fn make_path<'a>(actions: impl Iterator<Item = &'a str>) -> Path {
         })
         .flatten()
         .fold((HashSet::new(), HashMap::new()), |mut acc, p| {
-            acc.0.insert(p.0);
+            acc.0.insert(Point::from(p.0));
             acc.1
-                .entry(p.0)
+                .entry(Point::from(p.0))
                 .and_modify(|e| *e = cmp::min(*e, p.1))
                 .or_insert(p.1);
             acc
@@ -69,7 +102,7 @@ pub fn make_path<'a>(actions: impl Iterator<Item = &'a str>) -> Path {
 }
 
 pub fn part_1() -> u32 {
-    manhattan_distance(*closest_intersection(&DATA[0], &DATA[1]), (0, 0))
+    manhattan_distance(closest_intersection(&DATA[0], &DATA[1]), &ORIGIN)
 }
 
 pub fn part_2() -> u32 {
@@ -90,11 +123,11 @@ mod tests {
     fn example_1_0() {
         assert_eq!(
             manhattan_distance(
-                *closest_intersection(
-                    &make_path(String::from("R8,U5,L5,D3").split(",")),
-                    &make_path(String::from("U7,R6,D4,L4").split(","))
+                closest_intersection(
+                    &make_path(String::from("R8,U5,L5,D3").split(SEPARATOR)),
+                    &make_path(String::from("U7,R6,D4,L4").split(SEPARATOR))
                 ),
-                (0, 0)
+                &ORIGIN
             ),
             6
         )
@@ -104,11 +137,11 @@ mod tests {
     fn example_1_1() {
         assert_eq!(
             manhattan_distance(
-                *closest_intersection(
-                    &make_path(String::from("R75,D30,R83,U83,L12,D49,R71,U7,L72").split(",")),
-                    &make_path(String::from("U62,R66,U55,R34,D71,R55,D58,R83").split(","))
+                closest_intersection(
+                    &make_path(String::from("R75,D30,R83,U83,L12,D49,R71,U7,L72").split(SEPARATOR)),
+                    &make_path(String::from("U62,R66,U55,R34,D71,R55,D58,R83").split(SEPARATOR))
                 ),
-                (0, 0)
+                &ORIGIN
             ),
             159
         )
@@ -118,13 +151,16 @@ mod tests {
     fn example_1_2() {
         assert_eq!(
             manhattan_distance(
-                *closest_intersection(
+                closest_intersection(
                     &make_path(
-                        String::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51").split(",")
+                        String::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+                            .split(SEPARATOR)
                     ),
-                    &make_path(String::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7").split(","))
+                    &make_path(
+                        String::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7").split(SEPARATOR)
+                    )
                 ),
-                (0, 0)
+                &ORIGIN
             ),
             135
         )
@@ -134,8 +170,8 @@ mod tests {
     fn example_2_0() {
         assert_eq!(
             better_intersection(
-                &make_path(String::from("R8,U5,L5,D3").split(",")),
-                &make_path(String::from("U7,R6,D4,L4").split(","))
+                &make_path(String::from("R8,U5,L5,D3").split(SEPARATOR)),
+                &make_path(String::from("U7,R6,D4,L4").split(SEPARATOR))
             )
             .1,
             30
@@ -146,8 +182,8 @@ mod tests {
     fn example_2_1() {
         assert_eq!(
             better_intersection(
-                &make_path(String::from("R75,D30,R83,U83,L12,D49,R71,U7,L72").split(",")),
-                &make_path(String::from("U62,R66,U55,R34,D71,R55,D58,R83").split(","))
+                &make_path(String::from("R75,D30,R83,U83,L12,D49,R71,U7,L72").split(SEPARATOR)),
+                &make_path(String::from("U62,R66,U55,R34,D71,R55,D58,R83").split(SEPARATOR))
             )
             .1,
             610
@@ -158,8 +194,10 @@ mod tests {
     fn example_2_2() {
         assert_eq!(
             better_intersection(
-                &make_path(String::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51").split(",")),
-                &make_path(String::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7").split(","))
+                &make_path(
+                    String::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51").split(SEPARATOR)
+                ),
+                &make_path(String::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7").split(SEPARATOR))
             )
             .1,
             410
@@ -168,11 +206,11 @@ mod tests {
 
     #[bench]
     fn bench_part_1(b: &mut Bencher) {
-        b.iter(|| part_1());
+        b.iter(part_1);
     }
 
     #[bench]
     fn bench_part_2(b: &mut Bencher) {
-        b.iter(|| part_2());
+        b.iter(part_2);
     }
 }
