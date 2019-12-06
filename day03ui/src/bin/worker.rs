42e02142d32abb502d7fe2129ad5cd36e5a04485
vescoc <claudio.vesco@gmail.com>
2019-12-06 09:03:12 +0100

    feature fix for clippy

--- day03ui/src/bin/worker.rs
+++ day03ui/src/bin/worker.rs
@@ -1,7 +1,9 @@
+#[cfg(feature = "native-worker")]
 use yew::agent::Threaded;
 
 fn main() {
     yew::initialize();
+    #[cfg(feature = "native-worker")]
     day03ui::worker::Worker::register();
     yew::run_loop();
 }
