a17a281ecc44fc6c5ab9cd9f4917de908e2e0a35
vescoc <claudio.vesco@gmail.com>
2019-12-16 23:25:09 +0100

    day 16 part 2

--- day16/src/lib.rs
+++ day16/src/lib.rs
@@ -26,8 +26,16 @@ fn join(data: &[i32]) -> String {
         .collect()
 }
 
-fn phase(data: &[i32]) -> Vec<i32> {
+fn fft(data: &[i32]) -> Vec<i32> {
     let len = data.len();
+    let mut partial_sum = vec![0; len];
+
+    let mut sum = 0;
+    for i in (0..len).rev() {
+        sum += data[i];
+        partial_sum[i] = sum;
+    }
+
     let mut result = Vec::with_capacity(len);
 
     for i in 1..=len {
@@ -38,12 +46,22 @@ fn phase(data: &[i32]) -> Vec<i32> {
             let k = 4 * i * k;
             let idx = k + i - 1;
             if idx < len {
-                a += data[idx..(idx + i).min(len)].iter().sum::<i32>();
+                a += partial_sum[idx]
+                    - if idx + i >= len {
+                        0
+                    } else {
+                        partial_sum[idx + i]
+                    };
             }
 
             let idx = k + 3 * i - 1;
             if idx < len {
-                b += data[idx..(idx + i).min(len)].iter().sum::<i32>();
+                b += partial_sum[idx]
+                    - if idx + i >= len {
+                        0
+                    } else {
+                        partial_sum[idx + i]
+                    };
             }
         }
 
@@ -55,22 +73,22 @@ fn phase(data: &[i32]) -> Vec<i32> {
     result
 }
 
-fn solve_1(data: &[i32], count: usize) -> Vec<i32> {
+fn solve_1(data: &[i32]) -> String {
     let mut current = data.to_owned();
-    for _ in 0..count {
-        current = phase(&current);
+    for _ in 0..100 {
+        current = fft(&current);
     }
-    current
+
+    join(&current[0..8])
 }
 
-fn solve_2(data: &[i32], count: usize) -> String {
+fn solve_2(data: &[i32]) -> String {
     let mut current = std::iter::repeat(data.to_vec())
         .take(10_000)
         .flatten()
         .collect::<Vec<i32>>();
-    for _i in 0..count {
-        println!("{}", _i);
-        current = phase(&current);
+    for _ in 0..100 {
+        current = fft(&current);
     }
 
     let index = join(&data[0..7]).parse().expect("invalid number");
@@ -79,11 +97,11 @@ fn solve_2(data: &[i32], count: usize) -> String {
 }
 
 pub fn part_1() -> String {
-    join(&solve_1(&DATA, 100)[0..8])
+    solve_1(&DATA)
 }
 
 pub fn part_2() -> String {
-    solve_2(&DATA, 100)
+    solve_2(&DATA)
 }
 
 #[cfg(test)]
@@ -92,24 +110,24 @@ mod tests {
     use test::Bencher;
 
     #[test]
-    fn test_phase_1() {
-        assert_eq!(join(&phase(&parse("12345678"))), "48226158");
+    fn test_fft_1() {
+        assert_eq!(join(&fft(&parse("12345678"))), "48226158");
     }
 
     #[test]
-    fn test_phase_2() {
-        assert_eq!(join(&phase(&phase(&parse("12345678")))), "34040438");
+    fn test_fft_2() {
+        assert_eq!(join(&fft(&fft(&parse("12345678")))), "34040438");
     }
 
     #[test]
-    fn test_phase_failing() {
-        assert_eq!(join(&phase(&parse("48226158"))), "34040438");
+    fn test_fft_failing() {
+        assert_eq!(join(&fft(&parse("48226158"))), "34040438");
     }
 
     #[test]
     fn test_example_1_1() {
         assert_eq!(
-            join(&solve_1(&parse("80871224585914546619083218645595"), 100)[0..8]),
+            solve_1(&parse("80871224585914546619083218645595")),
             String::from("24176176")
         );
     }
@@ -117,7 +135,7 @@ mod tests {
     #[test]
     fn test_example_1_2() {
         assert_eq!(
-            join(&solve_1(&parse("19617804207202209144916044189917"), 100)[0..8]),
+            solve_1(&parse("19617804207202209144916044189917")),
             String::from("73745418")
         );
     }
@@ -125,7 +143,7 @@ mod tests {
     #[test]
     fn test_example_1_3() {
         assert_eq!(
-            join(&solve_1(&parse("69317163492948606335995924319873"), 100)[0..8]),
+            solve_1(&parse("69317163492948606335995924319873")),
             String::from("52432133")
         );
     }
@@ -133,7 +151,7 @@ mod tests {
     #[test]
     fn test_example_2_1() {
         assert_eq!(
-            solve_2(&parse("03036732577212944063491565474664"), 100),
+            solve_2(&parse("03036732577212944063491565474664")),
             String::from("84462026")
         );
     }
