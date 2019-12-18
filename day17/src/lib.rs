b14b9fc3f95a50f640a81f6f898508aeed094d76
vescoc <claudio.vesco@gmail.com>
2019-12-18 01:36:10 +0100

    day 17 part 2

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -33,12 +33,121 @@ fn solve_1(map: &str) -> i32 {
         .sum()
 }
 
+fn encode(s: &str) -> Result<String, ()> {
+    let mut r = s
+        .chars()
+        .fold(vec![], |mut acc, c| {
+            let mut v = match acc.pop() {
+                Some((l, n)) if l == c => vec![(l, n + 1)],
+                Some(v) => vec![v, (c, 1)],
+                _ => vec![(c, 1)],
+            };
+
+            acc.append(&mut v);
+            acc
+        })
+        .into_iter()
+        .try_fold(String::new(), |acc, (l, n)| match (l, n) {
+            ('F', n) => Ok(acc + &n.to_string() + ","),
+            ('L', 1) => Ok(acc + "L,"),
+            ('R', 1) => Ok(acc + "R,"),
+            _ => Err(()),
+        })?;
+
+    r.pop();
+
+    Ok(r)
+}
+
+fn find(s: &str) -> (String, String, String) {
+    for i in (0..s.len()).rev() {
+        let a = &s[0..i];
+        if encode(a).map(|s| s.len() <= 20).unwrap_or(false) {
+            let middle = s.split(a).collect::<String>();
+            for i in (0..middle.len()).rev() {
+                let b = &middle[0..i];
+                if encode(b).map(|s| s.len() <= 20).unwrap_or(false) {
+                    let last = middle.split(b).collect::<String>();
+                    for i in (0..last.len()).rev() {
+                        let c = &last[0..i];
+                        if encode(c).map(|s| s.len() <= 20).unwrap_or(false)
+                            && last.split(c).collect::<String>() == ""
+                        {
+                            return (a.to_string(), b.to_string(), c.to_string());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+fn solve_2(map: &str) -> intcode::Memory {
+    let moves = map
+        .parse::<path::Path>()
+        .unwrap()
+        .moves()
+        .iter()
+        .map(|m| match m {
+            path::Move::Forward => "F",
+            path::Move::Left => "L",
+            path::Move::Right => "R",
+        })
+        .collect::<String>();
+
+    let (a, b, c) = find(&moves);
+
+    let mut r = moves.replace(&a, "A,").replace(&b, "B,").replace(&c, "C,");
+
+    r.pop();
+
+    let tmp = r
+        + "\n"
+        + &encode(&a).unwrap()
+        + "\n"
+        + &encode(&b).unwrap()
+        + "\n"
+        + &encode(&c).unwrap()
+        + "\nn\n";
+
+    let mut input = tmp.chars();
+
+    let mut output = vec![];
+
+    let mut cpu = intcode::CPU::new(
+        PROGRAM
+            .iter()
+            .enumerate()
+            .map(|(i, v)| if i == 0 { 2 } else { *v })
+            .collect::<Vec<_>>(),
+        0,
+        None,
+    );
+    loop {
+        match cpu.run().expect("invalid state") {
+            intcode::Run::NeedInput => {
+                if let Some(c) = input.next() {
+                    cpu.set_input(Some(c as u8 as intcode::Memory));
+                } else {
+                    panic!("EOF");
+                }
+            }
+            intcode::Run::Output(value) => output.push(value),
+            intcode::Run::Halt => break,
+        }
+    }
+
+    output.last().unwrap().to_owned()
+}
+
 pub fn part_1() -> i32 {
     solve_1(&OUTPUT)
 }
 
-pub fn part_2() -> i64 {
-    unimplemented!()
+pub fn part_2() -> intcode::Memory {
+    solve_2(&OUTPUT)
 }
 
 #[cfg(test)]
@@ -59,6 +168,17 @@ mod tests {
     #[test]
     fn test_solve_1() {
         assert_eq!(solve_1(&EXAMPLE), 76);
+    }
+
+    #[test]
+    fn test_encode() {
+        assert_eq!(encode("LFFFRFFF").unwrap(), "L,3,R,3");
+    }
+
+    #[test]
+    fn test_split() {
+        assert_eq!("AAA".split("AAA").collect::<String>(), "");
+        assert_eq!("AAABBBAAA".split("AAA").collect::<String>(), "BBB");
     }
 
     #[bench]
--- day17/src/path.rs
+++ day17/src/path.rs
@@ -5,15 +5,23 @@ type Point = (i32, i32);
 
 #[derive(Copy, Clone, Debug)]
 enum Direction {
-    Up,
-    Down,
+    North,
+    South,
+    West,
+    East,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Move {
     Left,
     Right,
+    Forward,
 }
 
 #[derive(Debug)]
 pub struct Path {
     path: Vec<Point>,
+    moves: Vec<Move>,
     start_position: Point,
     start_direction: Direction,
 }
@@ -28,6 +36,10 @@ impl Path {
             })
             .into_iter()
             .filter_map(|(k, v)| if v > 1 { Some(k) } else { None })
+    }
+
+    pub fn moves(&self) -> &Vec<Move> {
+        &self.moves
     }
 }
 
@@ -51,22 +63,22 @@ impl FromStr for Path {
                             (_, '.') => Ok(()),
                             (None, '^') => {
                                 map.insert((x, y));
-                                position_info = Some(((x, y), Direction::Up));
+                                position_info = Some(((x, y), Direction::North));
                                 Ok(())
                             }
                             (None, '<') => {
                                 map.insert((x, y));
-                                position_info = Some(((x, y), Direction::Left));
+                                position_info = Some(((x, y), Direction::West));
                                 Ok(())
                             }
                             (None, 'v') => {
                                 map.insert((x, y));
-                                position_info = Some(((x, y), Direction::Down));
+                                position_info = Some(((x, y), Direction::South));
                                 Ok(())
                             }
                             (None, '>') => {
                                 map.insert((x, y));
-                                position_info = Some(((x, y), Direction::Right));
+                                position_info = Some(((x, y), Direction::East));
                                 Ok(())
                             }
                             (Some(_), c) if c == '^' || c == '<' || c == '>' || c == 'v' => {
@@ -81,6 +93,7 @@ impl FromStr for Path {
 
         if let Some((start_position, start_direction)) = position_info {
             let mut path = vec![];
+            let mut moves = vec![];
             let mut position = start_position;
             let mut direction = start_direction;
             loop {
@@ -88,19 +101,22 @@ impl FromStr for Path {
                 let target_position = next(position, direction);
                 if map.contains(&target_position) {
                     position = target_position;
+                    moves.push(Move::Forward);
                 } else {
-                    let mut v = choose_directions(direction).filter_map(|d| {
+                    let mut v = choose_directions(direction).filter_map(|(d, m)| {
                         let p = next(position, d);
                         if map.contains(&p) {
-                            Some((p, d))
+                            Some((p, d, m))
                         } else {
                             None
                         }
                     });
                     match (v.next(), v.next()) {
-                        (Some((new_position, new_direction)), None) => {
+                        (Some((new_position, new_direction, m)), None) => {
                             position = new_position;
                             direction = new_direction;
+                            moves.push(m);
+                            moves.push(Move::Forward);
                         }
                         (_, Some(_)) => panic!("path ambiguity"),
                         (None, None) => break,
@@ -110,6 +126,7 @@ impl FromStr for Path {
 
             Ok(Self {
                 path,
+                moves,
                 start_position,
                 start_direction,
             })
@@ -121,16 +138,34 @@ impl FromStr for Path {
 
 fn next(position: Point, direction: Direction) -> Point {
     match direction {
-        Direction::Up => (position.0, position.1 - 1),
-        Direction::Down => (position.0, position.1 + 1),
-        Direction::Left => (position.0 - 1, position.1),
-        Direction::Right => (position.0 + 1, position.1),
+        Direction::North => (position.0, position.1 - 1),
+        Direction::South => (position.0, position.1 + 1),
+        Direction::West => (position.0 - 1, position.1),
+        Direction::East => (position.0 + 1, position.1),
     }
 }
 
-fn choose_directions(direction: Direction) -> impl Iterator<Item = Direction> {
+fn choose_directions(direction: Direction) -> impl Iterator<Item = (Direction, Move)> {
     match direction {
-        Direction::Up | Direction::Down => vec![Direction::Left, Direction::Right].into_iter(),
-        Direction::Left | Direction::Right => vec![Direction::Up, Direction::Down].into_iter(),
+        Direction::North => vec![
+            (Direction::West, Move::Left),
+            (Direction::East, Move::Right),
+        ]
+        .into_iter(),
+        Direction::South => vec![
+            (Direction::West, Move::Right),
+            (Direction::East, Move::Left),
+        ]
+        .into_iter(),
+        Direction::West => vec![
+            (Direction::North, Move::Right),
+            (Direction::South, Move::Left),
+        ]
+        .into_iter(),
+        Direction::East => vec![
+            (Direction::North, Move::Left),
+            (Direction::South, Move::Right),
+        ]
+        .into_iter(),
     }
 }
