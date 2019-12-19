c425c5b8e4dcc6cc8fb41e6c6ff2b08587255565
vescoc <claudio.vesco@gmail.com>
2019-12-19 18:07:52 +0100

    day 19

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -10,32 +10,54 @@ lazy_static! {
     static ref PROGRAM: Vec<intcode::Memory> = intcode::parse(include_str!("../data.txt"));
 }
 
+fn check(x: usize, y: usize) -> bool {
+    let mut cpu = intcode::CPU::new(PROGRAM.to_vec(), 0, Some(x as intcode::Memory));
+
+    loop {
+        match cpu.run().expect("got error") {
+            intcode::Run::NeedInput => {
+                cpu.set_input(Some(y as intcode::Memory));
+                match cpu.run().expect("got error") {
+                    intcode::Run::Output(value) => {
+                        break match value {
+                            0 => false,
+                            1 => true,
+                            r => panic!("invalid response: {}", r),
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            s => panic!("invalid state: {:?} as ({}, {})", s, x, y),
+        }
+    }
+}
+
 pub fn part_1() -> usize {
-    (0 as intcode::Memory .. 50 as intcode::Memory)
-	.map(|x| {
-	    (0 as intcode::Memory .. 50 as intcode::Memory).map(|y| {
-		let mut cpu = intcode::CPU::new(PROGRAM.to_vec(), 0, Some(x));
-		
-		loop {
-		    match cpu.run().expect("got error") {
-			intcode::Run::NeedInput => {
-			    cpu.set_input(Some(y));
-			    match cpu.run().expect("got error") {
-				intcode::Run::Output(value) => break value as usize,
-				_ => unreachable!(),
-			    }
-			}
-			s => panic!("invalid state: {:?} as ({}, {})", s, x, y),
-		    }
-		}
-	    })
-		.sum::<usize>()
-	})
-	.sum()	     
+    (0..50)
+        .map(|x| {
+            (0..50)
+                .map(|y| if check(x, y) { 1 } else { 0 })
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 pub fn part_2() -> usize {
-    unimplemented!()
+    let (mut x, mut y) = (1, 1);
+    loop {
+        while !check(x + 99, y) {
+            y += 1;
+        }
+        while !check(x, y + 99) {
+            x += 1;
+        }
+        if check(x + 99, y) {
+            break x * 10000 + y;
+        } else {
+            x += 1
+        }
+    }
 }
 
 #[cfg(test)]
