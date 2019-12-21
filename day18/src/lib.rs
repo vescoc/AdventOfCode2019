faa753c6078a0fba0b6d06844b06d5dfcdc41a9b
vescoc <claudio.vesco@gmail.com>
2019-12-21 13:20:36 +0100

    day 18 (not working)

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -15,9 +15,15 @@ fn solve_1(data: &str) -> (usize, Vec<char>) {
         .parse::<vault::Vault>()
         .unwrap_or_else(|e| panic!("invalid data: {}", e));
 
-    let (moves, keys) = vault.search().unwrap();
+    println!("{}", vault);
 
-    (moves.len() - 1, keys)
+    if true {
+	unimplemented!()
+    } else {
+	let (moves, keys) = vault.search().unwrap();
+
+	(moves - 1, keys)
+    }
 }
 
 pub fn part_1() -> usize {
--- day18/src/vault.rs
+++ day18/src/vault.rs
@@ -1,10 +1,10 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashSet, HashMap, VecDeque};
 use std::fmt;
 use std::str::FromStr;
 
 type Point = (usize, usize);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 enum Tile {
     Empty,
     Wall,
@@ -17,6 +17,8 @@ pub struct Vault {
     start_position: Point,
     keys: Vec<char>,
     keys_set: HashSet<char>,
+    key_positions: HashMap<Tile, Point>,
+    door_positions: HashMap<Tile, Point>,
 }
 
 impl Vault {
@@ -32,71 +34,66 @@ impl Vault {
         self.tiles.get(y).and_then(|v| v.get(x).copied())
     }
 
-    fn find_paths_to_keys(
-        &self,
-        start_position: &Point,
-        discovered_keys: &HashSet<char>,
-	prune: usize,
-    ) -> Vec<(char, Vec<Point>)> {
-        let mut remaining_keys = self
-            .keys_set
-            .difference(discovered_keys)
-            .copied()
-            .collect::<HashSet<char>>();
+    fn find_paths_from(
+	&self,
+	start_position: &Point,
+    ) -> Vec<(char, HashSet<char>, HashSet<char>, usize)> {
+	let mut remaining_keys = self.keys_set.to_owned();
 
-        let mut result = vec![];
+	let mut result = vec![];
 
-        let mut queue = VecDeque::new();
-        let mut visited = HashSet::new();
+	let mut queue = VecDeque::new();
+	let mut visited = HashSet::new();
 
-        let next = |(x, y), visited: &HashSet<Point>, remaining_keys: &HashSet<char>| {
-            vec![(1, 0), (-1, 0), (0, 1), (0, -1)]
-                .iter()
-                .map(|(dx, dy)| ((x as i128 + dx) as usize, (y as i128 + dy) as usize))
-                .filter(|p| !visited.contains(p))
-                .filter_map(|(x, y)| match self.get(x, y) {
-                    Some(Tile::Empty) => Some(((x, y), None)),
-                    Some(Tile::Door(c)) if discovered_keys.contains(&c) => Some(((x, y), None)),
-                    Some(Tile::Key(c)) => Some((
-                        (x, y),
-                        if remaining_keys.contains(&c) {
-                            Some(c)
-                        } else {
-                            None
-                        },
-                    )),
-                    _ => None,
-                })
-                .collect::<Vec<_>>()
-                .into_iter()
-        };
+	let next = |(x, y), visited: &HashSet<Point>| {
+	    vec![(1, 0), (-1, 0), (0, 1), (0, -1)]
+		.iter()
+		.map(|(dx, dy)| ((x as i128 + dx) as usize, (y as i128 + dy) as usize))
+		.filter(|p| !visited.contains(p))
+		.filter_map(|(x, y)| match self.get(x, y) {
+		    Some(Tile::Empty) => Some(((x, y), None)),
+		    Some(t @ Tile::Door(_)) => Some(((x, y), Some(t))),
+		    Some(t @ Tile::Key(_)) => Some(((x, y), Some(t))),
+		    _ => None,			 
+		})
+		.collect::<Vec<_>>()
+		.into_iter()
+	};
 
-        queue.push_back((*start_position, vec![]));
+	let mut keys = HashSet::new();
+	let mut doors = HashSet::new();
+
+	match self.get(start_position.0, start_position.1) {
+	    Some(Tile::Key(k)) => { keys.insert(k); }
+	    Some(Tile::Door(d)) => { doors.insert(d); }
+	    _ => {}
+	}
+
+	queue.push_back((*start_position, keys, doors, 0));
         loop {
             if remaining_keys.is_empty() {
                 break result;
-            } else if let Some((position, moves)) = queue.pop_front() {
+            } else if let Some((position, keys, doors, moves)) = queue.pop_front() {
                 visited.insert(position);
-                next(position, &visited, &remaining_keys).for_each(|(new_position, new_key)| {
-                    match new_key {
-                        Some(new_key) => {
-                            result.push({
-                                let mut moves = moves.to_owned();
-                                moves.push(new_position);
-                                (new_key, moves)
-                            });
+                next(position, &visited).for_each(|(new_position, tile)| {
+                    match tile {
+			Some(Tile::Key(k)) => {
+			    let mut keys = keys.to_owned();
+			    keys.insert(k);
+			    
+			    result.push((k, keys.to_owned(), doors.to_owned(), moves + 1));
 
-                            remaining_keys.remove(&new_key);
-                        }
-                        None => {
-			    if moves.len() + 1 < prune {
-				queue.push_back((new_position, {
-                                    let mut moves = moves.to_owned();
-                                    moves.push(new_position);
-                                    moves
-				}));
-			    }
-                        }
+			    remaining_keys.remove(&k);
+
+			    queue.push_back((new_position, keys, doors.to_owned(), moves + 1));
+			}
+			Some(Tile::Door(d)) => {
+			    let mut doors = doors.to_owned();
+			    doors.insert(d);
+
+			    queue.push_back((new_position, keys.to_owned(), doors.to_owned(), moves + 1));
+			}
+                        _ => queue.push_back((new_position, keys.to_owned(), doors.to_owned(), moves + 1)),
                     }
                 });
             } else {
@@ -105,41 +102,8 @@ impl Vault {
         }
     }
 
-    pub fn search(&self) -> Result<(Vec<Point>, Vec<char>), String> {
-	let mut candidate: Option<(Vec<Point>, Vec<char>)> = None;
-	let mut queue = vec![]; // VecDeque::new();
-
-	queue.push((vec![self.start_position], vec![]));
-	loop {
-	    if let Some((path, discovered_keys)) = queue.pop() {
-		let prune = candidate.as_ref().map(|(v, _)| v.len()).unwrap_or(std::usize::MAX);
-		let path_len = path.len();
-		//println!("{} [{}] {:?} [{}]", prune, path_len, discovered_keys, queue.len());
-		if path_len < prune {
-		    let discovered_keys_set = discovered_keys.iter().copied().collect::<HashSet<char>>();
-		    for (c, mut path_to) in self.find_paths_to_keys(path.last().unwrap(), &discovered_keys_set, prune - path_len) {
-			let mut discovered_keys = discovered_keys.to_owned();
-			discovered_keys.push(c);
-
-			let mut discovered_keys_set = discovered_keys_set.to_owned();
-			discovered_keys_set.insert(c);
-
-			let mut path = path.to_owned();
-			path.append(&mut path_to);
-			
-			if self.keys_set == discovered_keys_set {
-			    candidate = Some((path, discovered_keys));
-			} else {
-			    queue.push((path, discovered_keys));
-			}
-		    }
-		}
-	    } else {
-		break;
-	    }
-	}
-
-	candidate.ok_or_else(|| String::from("no solution!"))
+    pub fn search(&self) -> Result<(usize, Vec<char>), String> {
+	unimplemented!()
     }
 }
 
@@ -159,7 +123,9 @@ impl fmt::Display for Vault {
         }
 
         writeln!(fmt, "start position: {:?}", self.start_position)?;
-        write!(fmt, "keys: {:?}", self.keys)
+        writeln!(fmt, "keys: {:?}", self.keys)?;
+	writeln!(fmt, "key positions: {:?}", self.key_positions)?;
+	writeln!(fmt, "door position: {:?}", self.door_positions)
     }
 }
 
@@ -167,7 +133,7 @@ impl FromStr for Vault {
     type Err = String;
 
     fn from_str(data: &str) -> Result<Self, String> {
-        let (tiles, start_position, mut keys) = data
+        let (tiles, start_position, mut keys, door_positions, key_positions) = data
             .lines()
             .enumerate()
             .map(|(y, l)| {
@@ -175,59 +141,48 @@ impl FromStr for Vault {
                     .chars()
                     .enumerate()
                     .map(|(x, c)| match c {
-                        '#' => Ok((Tile::Wall, None, None)),
-                        '.' => Ok((Tile::Empty, None, None)),
-                        'a'..='z' => Ok((Tile::Key(c), None, Some(c))),
-                        'A'..='Z' => Ok((Tile::Door(c.to_ascii_lowercase()), None, None)),
-                        '@' => Ok((Tile::Empty, Some((x, y)), None)),
+                        '#' => Ok((Tile::Wall, (x, y), None)),
+                        '.' => Ok((Tile::Empty, (x, y), None)),
+                        'a'..='z' => Ok((Tile::Key(c), (x, y), None)),
+                        'A'..='Z' => Ok((Tile::Door(c.to_ascii_lowercase()), (x, y), None)),
+                        '@' => Ok((Tile::Empty, (x, y), Some((x, y)))),
                         _ => Err(format!("invalid char {} at ({}, {})", c, x, y)),
                     })
                     .try_fold(
-                        (vec![], None, vec![]),
-                        |(mut v, position, mut keys), r| match r {
-                            Ok((t, p, key)) => match (p, position, key) {
-                                (Some(_), Some(_), _) => Err(format!(
-                                    "duplicate start position, now {:?}, old {:?}",
-                                    p, position
-                                )),
-                                (Some(_), None, key) => {
-                                    v.push(t);
-                                    if let Some(key) = key {
-                                        keys.push(key);
-                                    }
-                                    Ok((v, p, keys))
-                                }
-                                (None, _, key) => {
-                                    v.push(t);
-                                    if let Some(key) = key {
-                                        keys.push(key);
-                                    }
-                                    Ok((v, position, keys))
-                                }
-                            },
+                        (vec![], None, vec![], vec![], vec![]),
+                        |(mut v, position, mut keys, mut door_positions, mut key_positions), r| match r {
+                            Ok((t, p, sp)) => {
+				match t {
+				    Tile::Key(k) => {
+					keys.push(k);
+					key_positions.push((t, p));
+				    }
+				    Tile::Door(_) => {
+					door_positions.push((t, p));
+				    }
+				    _ => {},
+				}
+				v.push(t);
+				Ok((v, position.or(sp), keys, door_positions, key_positions))
+			    }
                             Err(e) => Err(e),
                         },
                     )
             })
             .try_fold(
-                (vec![], None, vec![]),
-                |(mut v, position, mut keys), r| match r {
-                    Ok((t, p, mut ks)) => match (p, position) {
-                        (Some(_), Some(_)) => Err(format!(
-                            "duplicate start position, now {:?}, old {:?}",
-                            p, position
-                        )),
-                        (Some(_), None) => {
-                            v.push(t);
-                            keys.append(&mut ks);
-                            Ok((v, p, keys))
-                        }
-                        (None, _) => {
-                            v.push(t);
-                            keys.append(&mut ks);
-                            Ok((v, position, keys))
-                        }
-                    },
+                (vec![], None, vec![], vec![], vec![]),
+                |(mut tiles, position, mut keys, mut door_positions, mut key_positions), r| match r {
+                    Ok((ts, p, mut k, mut dp, mut kp)) => {
+			tiles.push(ts);
+			keys.append(&mut k);
+			door_positions.append(&mut dp);
+			key_positions.append(&mut kp);
+			Ok((tiles,
+			    if position.is_some() { position } else { p },
+			    keys,
+			    door_positions,
+			    key_positions))
+		    }
                     Err(e) => Err(e),
                 },
             )?;
@@ -240,6 +195,8 @@ impl FromStr for Vault {
                 .ok_or_else(|| String::from("cannot find start_position"))?,
             keys: keys.to_owned(),
 	    keys_set: keys.into_iter().collect(),
+	    door_positions: door_positions.into_iter().collect(),
+	    key_positions: key_positions.into_iter().collect(),
         })
     }
 }
@@ -249,7 +206,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_find_path_simple() {
+    fn test_find_paths_from() {
         let vault = r"#########
 #b.A.@.a#
 #########"
@@ -257,58 +214,8 @@ mod tests {
             .unwrap();
 
         assert_eq!(
-            vault.find_paths_to_keys(&vault.start_position,
-				     &HashSet::new(),
-				     std::usize::MAX),
-            vec![('a', vec![(6, 1), (7, 1)])],
-        )
-    }
-
-    #[test]
-    fn test_find_path_discovered() {
-        let vault = r"#########
-#b.A.@.a#
-#########"
-            .parse::<Vault>()
-            .unwrap();
-
-        assert_eq!(
-            vault.find_paths_to_keys(&vault.start_position,
-				     &['a'].iter().copied().collect(),
-				     std::usize::MAX),
-            vec![('b', vec![(4, 1), (3, 1), (2, 1), (1, 1)])],
-        )
-    }
-
-    #[test]
-    fn test_find_path_prune() {
-        let vault = r"#########
-#b.A.@.a#
-#########"
-            .parse::<Vault>()
-            .unwrap();
-
-        assert_eq!(
-            vault.find_paths_to_keys(&vault.start_position,
-				     &['a'].iter().copied().collect(),
-				     3),
-            Vec::<(char, Vec<_>)>::new(),
-        )
-    }
-
-    #[test]
-    fn test_find_path_prune_hit() {
-        let vault = r"#########
-#b.A.@.a#
-#########"
-            .parse::<Vault>()
-            .unwrap();
-
-        assert_eq!(
-            vault.find_paths_to_keys(&vault.start_position,
-				     &['a'].iter().copied().collect(),
-				     5),
-            vec![('b', vec![(4, 1), (3, 1), (2, 1), (1, 1)])],
+            vault.find_paths_from(&vault.start_position),
+            vec![('a', vec!['a'].into_iter().collect(), HashSet::new(), 2), ('b', vec!['b'].into_iter().collect(), vec!['a'].into_iter().collect(), 4)],
         )
     }
 }
